e055836a5d839926b2c505ad637cac0a49687255
celil-vural <celilvural.work@gmail.com>
2023-09-26 21:08:50 +0300

    update

--- spring-example/spring-boot-jwt/src/main/java/com/springbootjwt/config/JwtService.java
+++ spring-example/spring-boot-jwt/src/main/java/com/springbootjwt/config/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
